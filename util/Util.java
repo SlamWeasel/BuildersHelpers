0d60ea05b6a6689071cbfb5ed626dbae084ddc25
SlamWeasel <63350349+SlamWeasel@users.noreply.github.com>
2021-10-23 01:34:27 +0200

    Utility Class

---
+++ util/Util.java
@@ -0,0 +1,12 @@
+package de.SlamWeasel.BuildersHelpers.util;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+
+public class Util
+{
+    public static final int[] Reihe = {0,9,18,27,36,45,54,63,72,81};
+    public static HashMap<Player, Location> BeenLoc = new HashMap<Player, Location>();
+}
